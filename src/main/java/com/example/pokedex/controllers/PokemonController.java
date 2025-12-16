6a65c38d30003390441935d6d6bed554a1eb1f7e
Marius Pascal <marius.pascal2b@gmail.com>
2025-12-16 10:26:08 +0100

    Ajout de la classe Pokemon

---
+++ src/main/java/com/example/pokedex/controllers/PokemonController.java
@@ -0,0 +1,5 @@
+package com.example.pokedex.controllers;
+
+public class PokemonController {
+    
+}
--- src/main/java/com/example/pokedex/models/Pokemon.java
+++ src/main/java/com/example/pokedex/models/Pokemon.java
@@ -1,5 +1,37 @@
 package com.example.pokedex.models;
 
 public class Pokemon {
-    
+    private int id;
+    private String name;
+    private String description;
+    private int height;
+    private int weight;
+
+    public Pokemon(int id, String name, String description, int height, int weight) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
