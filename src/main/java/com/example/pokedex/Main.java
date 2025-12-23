ff3c6361220c64a57d267c06391e82a894d4bd01
Anaïs Dubois <anais.dubois5@outlook.fr>
2025-12-23 11:53:09 +0100

    Modification imports dans Main.java

--- src/main/java/com/example/pokedex/Main.java
+++ src/main/java/com/example/pokedex/Main.java
@@ -1,9 +1,6 @@
 package com.example.pokedex;
 
 
-import com.example.codesamples.JSONExample;
-import com.example.codesamples.SQLiteExample;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         PokedexRunner runner = new PokedexRunner();
